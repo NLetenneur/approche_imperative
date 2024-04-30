f1a498b527272c4b970aaae14c8ef6fce58c7416
Noémie Letenneur <n.letenneur@gmail.com>
2024-04-30 10:05:22 +0200

    TP6 ex01

---
+++ src/main/java/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,20 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+
+	public static void main(String[] args) {
+		for (int i=1; i<11; i++) {
+			System.out.println(i);
+		}
+		for (int i=0; i<20; i++) {
+			System.out.println("Letenneur Noémie");
+		}
+		for (int i=2; i<101; i=i+2) {
+			System.out.println(i);
+		}
+		for (int i=1; i<100; i=i+2) {
+			System.out.println(i);
+		}
+	}
+
+}
